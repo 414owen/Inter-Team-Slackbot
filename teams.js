b032c9ae0f5994682efb24339949817e23ae579e
Owen Shepherd <414owen@gmail.com>
2017-05-25 00:42:00 +0100

    Better teams.js example

--- teams.js
+++ teams.js
@@ -1,8 +1,23 @@
 var teams = [
+	// Team One
+	{
+		token: "xxxx-xxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxx",
+		channel: "team-link"
+	},
+
+	// Team Two
+	{
+		token: "xxxx-xxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxx",
+		channel: "interteam"
+	},
+
+	// Team Three
 	{
 		token: "xxxx-xxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxx",
 		channel: "general"
-	},
+	}
+	
+	// ...etc
 ];
 
 module.exports = teams;
